6cdb91dcb2a0572e31872551c098e707a02f92c0
Kevin Scroggins <nitro404@gmail.com>
2026-01-05 16:40:47 -0500

    Update Crypto++ library to support CMake 4.2.1

--- cryptopp-config.cmake
+++ cryptopp-config.cmake
@@ -1,1 +1,7 @@
-include("${CMAKE_CURRENT_LIST_DIR}/cryptopp-targets.cmake")
+if(BUILD_SHARED)
+	include("${CMAKE_CURRENT_LIST_DIR}/cryptopp-targets-shared.cmake")
+endif()
+
+if(BUILD_STATIC)
+	include("${CMAKE_CURRENT_LIST_DIR}/cryptopp-targets-static.cmake")
+endif()
